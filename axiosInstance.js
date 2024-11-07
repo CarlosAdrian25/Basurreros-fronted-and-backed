ffc15d20caf5163e5a2d56ad8db16aa1470f9eed
Adrian Flores <140359838+CarlosAdrian25@users.noreply.github.com>
2024-11-07 10:36:02 -0400

    Delete axiosInstance.js

--- axiosInstance.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5173/api', // URL base del backend de Laravel
-    withCredentials: true, // Esto asegura que se envíen cookies (sesión)
-});
-
-export default axiosInstance;
